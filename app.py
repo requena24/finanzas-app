f14cff45076457c0d6e2f23c04967035772606ec
Roberto Requena <119118571+requena24@users.noreply.github.com>
2025-04-13 10:14:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ for idx, row in df.iterrows():
 
     # Mostramos cada movimiento como una línea con resumen e ID
     seleccionado = st.checkbox(
-        f"{row['fecha']} - {row['tipo']} - ${row['monto']} - {row['categoria']}",
+        f"{row['Fecha']} - {row['Tipo']} - ${row['Monto']} - {row['Categoría']}",
         key=checkbox_key
     )
 
